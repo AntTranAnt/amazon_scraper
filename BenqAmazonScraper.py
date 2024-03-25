244bcc0dd482876a6184d853684268a5c7a42178
AntTranAnt <tranant2002@gmail.com>
2024-03-25 10:22:05 -0500

    changed workflow steps

--- BenqAmazonScraper.py
+++ BenqAmazonScraper.py
@@ -1,11 +1,26 @@
 #Extended class for BenQ scraping
 #Takes in excel file and scrapes each ASIN to find company name and price
+import pandas as pd
 
 from AmazonScraper import AmazonScraper
 
 class BenqAmazonScraper(AmazonScraper):
     fileName: str
+    dataframe: pd
 
     # Method to set file name of import excel file to search
     def setFileName(self, fileName):
         self.fileName = fileName
+    
+    #Method to read excel file to data frame
+    # Make sure in external program to call setFileName first
+    def readFile(self):
+        self.dataframe = pd.read_excel(self.fileName)
+    
+    # Method to search each ASIN for company name and price
+    # Adds to dataframe property
+    def searchASIN(self):
+        
+
+    def exportExcel(self):
+        
