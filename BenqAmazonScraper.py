34dc8138b6d86006440b7c2e16f998f2e3489639
AntTranAnt <tranant2002@gmail.com>
2024-03-27 16:24:09 -0500

    added steps to BenQAmazonScraper

--- BenqAmazonScraper.py
+++ BenqAmazonScraper.py
@@ -8,19 +8,23 @@ class BenqAmazonScraper(AmazonScraper):
     fileName: str
     dataframe: pd
 
-    # Method to set file name of import excel file to search
+    # Use the methods in linear order
+
+    # 1. Method to set file name of import excel file to search
     def setFileName(self, fileName):
         self.fileName = fileName
     
-    #Method to read excel file to data frame
+    # 2. Method to read excel file to data frame
     # Make sure in external program to call setFileName first
     def readFile(self):
         self.dataframe = pd.read_excel(self.fileName)
     
-    # Method to search each ASIN for company name and price
+    # 3. Method to search each ASIN for company name and price
     # Adds to dataframe property
     def searchASIN(self):
-        
+        # https://www.amazon.com/dp/
+        baseLink = "https://www.amazon/dp/"
 
+    # 4. Export database to excel file
     def exportExcel(self):
-        
+        # do i call exportExcel from superclass?
