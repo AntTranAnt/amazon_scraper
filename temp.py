f95b9868946f006ab364caed92693ad82bc4d55c
AntTranAnt <tranant2002@gmail.com>
2024-04-05 11:29:52 -0500

    Found out how to scrape Ships From and Sold By

--- temp.py
+++
@@ -1,7 +0,0 @@
-from BenqAmazonScraper import BenqAmazonScraper
-
-benqScraper = BenqAmazonScraper()
-benqScraper.setFileName("test.xlsx")
-benqScraper.readFile()
-benqScraper.searchASIN()
-print(benqScraper.dataframe)
